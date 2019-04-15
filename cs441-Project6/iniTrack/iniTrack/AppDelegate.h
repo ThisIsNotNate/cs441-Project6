559d170e78457c570d80f2c9dea349fc7ddcec29
Nathaniel Hilscher <nhilsch1@bl-n03.pods.bu.int>
2019-04-15 16:26:59 -0400

    The nuclear option

---
+++ cs441-Project6/iniTrack/iniTrack/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iniTrack
+//
+//  Created by Nathaniel Hilscher on 4/15/19.
+//  Copyright © 2019 Nathaniel Hilscher. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
