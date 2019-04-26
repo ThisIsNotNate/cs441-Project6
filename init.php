8fdca411ff21a68f3a30690c9bad14bc95a8f71c
George Milouchev <geoman362@gmail.com>
2019-04-26 17:01:23 -0400

    Upgraded actually working php files hooray

--- init.php
+++ init.php
@@ -4,16 +4,24 @@
 $dbname = "gmilouc1_initiative";
 $dbpwd = "miph6Saezook";
 $dbuser = "gmilouc1";
+
+
+
+$game_id = $_GET["game_id"]; //parse_url($_SERVER['REQUEST_URI'], PHP_URL_QUERY);
+$game_id = str_replace("/", "", $game_id);
+
+//echo $game_id;
  
 $host = "mysql.cs.binghamton.edu";
 $cid = mysqli_connect($host, $dbuser, $dbpwd, $dbname);
  
-$sql = "select * from players";
+$sql = "select * from players where game_id LIKE '".$game_id."' order by initiative desc";
+//echo $sql;
+//$sql = "select * from players";
 $result = mysqli_query($cid, $sql);
 while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC))
 {
- $nm = $row["name"];
- $hp = $row["current_health"];
+ 
 
  echo json_encode($row);
 }
